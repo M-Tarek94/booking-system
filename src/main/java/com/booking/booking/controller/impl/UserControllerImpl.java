35948200ef2994ea1330c7e541a41ad326324038
Mohamed Tarek <mohamed.tarek4@vodafone.com>
2025-02-19 14:52:14 +0200

    base structure

---
+++ src/main/java/com/booking/booking/controller/impl/UserControllerImpl.java
@@ -0,0 +1,9 @@
+package com.booking.booking.controller.impl;
+
+import com.booking.booking.controller.UserController;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserControllerImpl implements UserController {
+
+}
